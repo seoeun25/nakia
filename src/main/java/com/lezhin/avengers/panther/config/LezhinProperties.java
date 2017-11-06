fc624b94dc867a5c8a778baae1b7c126e3bb106a
azrael.seoeun <seoeun25@gmail.com>
2017-11-07 01:03:23 +0900

    Add properties

---
+++ src/main/java/com/lezhin/avengers/panther/config/LezhinProperties.java
@@ -0,0 +1,72 @@
+package com.lezhin.avengers.panther.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author seoeun
+ * @since 2017.11.06
+ */
+@Configuration
+@ConfigurationProperties("lezhin")
+public class LezhinProperties {
+
+    private String webUrl;
+    private String apiUrl;
+    private String pantherUrl;
+    private Happypoint happypoint;
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+
+    public String getApiUrl() {
+        return apiUrl;
+    }
+
+    public void setApiUrl(String apiUrl) {
+        this.apiUrl = apiUrl;
+    }
+
+    public String getPantherUrl() {
+        return pantherUrl;
+    }
+
+    public void setPantherUrl(String pantherUrl) {
+        this.pantherUrl = pantherUrl;
+    }
+
+    public Happypoint getHappypoint() {
+        return happypoint;
+    }
+
+    public void setHappypoint(Happypoint happypoint) {
+        this.happypoint = happypoint;
+    }
+
+    public static class Happypoint {
+        private String hpcUrl;
+        private String mchtNo;
+
+        public String getHpcUrl() {
+            return hpcUrl;
+        }
+
+        public void setHpcUrl(String hpcUrl) {
+            this.hpcUrl = hpcUrl;
+        }
+
+        public String getMchtNo() {
+            return mchtNo;
+        }
+
+        public void setMchtNo(String mchtNo) {
+            this.mchtNo = mchtNo;
+        }
+
+    }
+}
