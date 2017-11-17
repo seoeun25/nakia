216f8c2d52bce44815ba725946df64c5880a01d4
azrael.seoeun <seoeun25@gmail.com>
2017-11-17 18:27:03 +0900

    Add build_local script

---
+++ build_local.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+## build. create docker image and push to docker repo(docker.lezhin.com)
+help="./build_locasl.sh <version>"
+
+VERSION=$1
+if [ "$VERSION" == "--help" ]
+then
+      echo "$help"
+      exit 0;
+fi
+
+CMD="CONTAINER_VERSION=${VERSION} CONTAINER_PUSH=1 gradle docker"
+echo "$CMD"
+echo ""
+
+CONTAINER_VERSION=${VERSION} CONTAINER_PUSH=1 gradle docker
+
