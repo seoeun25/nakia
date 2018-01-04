d5844cc02ded2bf9682ce8a2a2be45057db48a26
azrael.seoeun <seoeun25@gmail.com>
2018-01-04 17:45:56 +0900

    DEV-10064 Handle exception at PageController

--- src/main/java/com/lezhin/panther/controller/PageController.java
+++ src/main/java/com/lezhin/panther/controller/PageController.java
@@ -1,12 +1,20 @@
 package com.lezhin.panther.controller;
 
 import com.lezhin.panther.config.PantherProperties;
+import com.lezhin.panther.executor.Executor;
+import com.lezhin.panther.lguplus2.Lguplus2Executor;
+import com.lezhin.panther.lguplus2.Lguplus2Payment;
+import com.lezhin.panther.model.PGPayment;
+import com.lezhin.panther.model.Payment;
+import com.lezhin.panther.model.RequestInfo;
+import com.lezhin.panther.util.JsonUtil;
 import com.lezhin.panther.util.Util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +23,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Page Controller. /webapp/WEB-INF/jsp/에 있는 jsp 를 리턴.
@@ -28,19 +38,27 @@ public class PageController {
     private static final Logger logger = LoggerFactory.getLogger(PageController.class);
 
     private PantherProperties pantherProperties;
+    private PagePayService pagePayService;
+    private SlackNotifier slackNotifier;
 
-    public PageController(final PantherProperties pantherProperties) {
+
+    public PageController(final PantherProperties pantherProperties, final PagePayService pagePayService,
+                          final SlackNotifier slackNotifier) {
         this.pantherProperties = pantherProperties;
+        this.pagePayService = pagePayService;
+        this.slackNotifier = slackNotifier;
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String hello(HttpServletRequest request, HttpServletResponse response, Model model) {
+    public ModelAndView hello(HttpServletRequest request, HttpServletResponse response) {
 
         String version = Optional.ofNullable(Util.loadVersion()).orElse("NULL");
-        model.addAttribute("name", "there, This is panther");
-        model.addAttribute("version", version);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name", "NEW there, This is panther");
+        modelAndView.addObject("version", version);
+        modelAndView.setViewName("hello");
 
-        return "hello";
+        return modelAndView;
     }
 
     @RequestMapping(value = "/{pg}/sample", method = RequestMethod.GET)
@@ -91,6 +109,17 @@ public class PageController {
 
         logger.info("payment. payres. pg = {}, pageKey = {}", pg, pageKey);
 
+        Map<String, String[]> params = new HashMap(request.getParameterMap());
+        Map<String, Object> newParams = params.entrySet().stream().collect(Collectors.toMap(e -> e.getKey(), e -> e
+                .getValue()[0]));
+
+
+
+
+        params.entrySet().stream().forEach(e -> logger.info("param. {} = {}", e.getKey(), e.getValue()[0]));
+        Lguplus2Payment pgPayment = JsonUtil.fromMap(newParams, Lguplus2Payment.class);
+        logger.info("lgu. Payement = {}", pgPayment.toString());
+
         String confDirStr = pantherProperties.getLguplus2().getConfDir();
         model.addAttribute("confDir", confDirStr); // for lguplus2
         File file = new File(confDirStr);
@@ -133,5 +162,63 @@ public class PageController {
 
         return pageKey;
 
+    }
+
+    @ExceptionHandler(InternalPaymentException.class)
+    public ModelAndView handleInternalPaymentExceptionException(HttpServletRequest request,
+                                                                InternalPaymentException e) {
+        logger.error("panther error. requestedUrl = " + request.getRequestURI(), e);
+        slackNotifier.notify(SlackEvent.builder()
+                .header(e.getType().name())
+                .level(SlackMessage.LEVEL.ERROR)
+                .title("Unexpected error")
+                .message(e.getMessage())
+                .exception(e)
+                .build());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e);
+        modelAndView.addObject("url", request.getRequestURL());
+
+        modelAndView.setViewName("error_internal");
+        return modelAndView;
+    }
+
+    @ExceptionHandler({ExecutorException.class, PantherException.class})
+    public ModelAndView handleException(HttpServletRequest request, Exception e) {
+        logger.error("panther error. requestedUrl = " + request.getRequestURI(), e);
+        slackNotifier.notify(SlackEvent.builder()
+                .header(((PantherException) e).getType().name())
+                .level(SlackMessage.LEVEL.ERROR)
+                .title("Unexpected error")
+                .message(e.getMessage())
+                .exception(e)
+                .build());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e);
+        modelAndView.addObject("url", request.getRequestURL());
+
+        modelAndView.setViewName("error_page");
+        return modelAndView;
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ModelAndView handleUnexpectedException(HttpServletRequest request, Exception e) {
+        logger.error("unexpected error. requestedUrl = " + request.getRequestURI(), e);
+        slackNotifier.notify(SlackEvent.builder()
+                .header(Executor.Type.DUMMY.name())
+                .level(SlackMessage.LEVEL.ERROR)
+                .title("Unexpected error")
+                .message(e.getMessage())
+                .exception(e)
+                .build());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e);
+        modelAndView.addObject("url", request.getRequestURL());
+
+        modelAndView.setViewName("error_page");
+        return modelAndView;
     }
 }
